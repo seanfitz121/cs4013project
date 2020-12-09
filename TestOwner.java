437d47631a692b23b3bf45b6b0503b8d33301012
ColumMaher <55181702+ColumMaher@users.noreply.github.com>
2020-12-09 16:34:03 +0000

    Update TestOwner.java

--- TestOwner.java
+++ TestOwner.java
@@ -1,23 +1,23 @@
-
 public class TestOwner
 {
     public TestOwner(){
     }
-    public void testPassedOwner(Owner Sam){
+    public void testPassedOwner(Owner o){
         Property LK = new Property("19 Cul Crannagh", "V94WCV9", 1000, "City", true, 2018);
-        //Property L = new Property("1 Fedamore", "V94KL01", 500, "Village", true, 2015);
+        Property L = new Property("1 Fedamore", "V94KL01", 500, "Village", true, 2015);
         //Property WE = new Property("18 Darrowton", "V94Y2VD", 2000, "Countryside", false, 2017);
-        Sam.registerProperty(LK);
-        //Sam.registerProperty(L);
+        o.registerProperty(LK);
+        o.registerProperty(L);
         //Sam.registerProperty(WE);
-        Sam.viewProperties();
-        Sam.viewPaidTax();
-        Sam.payTax(LK, 2020);
-        Sam.payTax(LK, 2018);
-        Sam.viewPaidTax();
-        Sam.viewDueTax();
-        Sam.viewOverdueTax();
-        Sam.balancingStatement(2020);
-        Sam.balancingStatement(LK);
+        o.viewProperties();
+        o.viewPaidTax();
+        o.payTax(LK, 2020);
+        o.payTax(L, 2018);
+        o.payTax(LK, 2018);
+        o.viewPaidTax();
+        o.viewDueTax();
+        o.viewOverdueTax();
+        o.balancingStatement(2020);
+        o.balancingStatement(LK);
     }
 }
