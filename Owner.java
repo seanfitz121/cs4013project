74b725c16696f66696e61cc3c79994be5f668c24
ColumMaher <55181702+ColumMaher@users.noreply.github.com>
2020-12-09 13:10:37 +0000

    Update Owner.java

--- Owner.java
+++ Owner.java
@@ -1,10 +1,10 @@
+import java.io.*;
 import java.util.ArrayList;
 
-public class Owner{
+public class Owner {
     private String name;
     /*List of properties*/
     private ArrayList<Property> properties = new ArrayList<>();
-    private ArrayList<Double> payments = new ArrayList<>();
     public Owner(String name){
         this.name = name;
     }
@@ -19,77 +19,99 @@ public class Owner{
         properties.add(p);
         System.out.println(p.getAddress() + " Registered");
     }
-    public void viewProperty(){
-        /* Return properties arraylist in readable format*/
-        /* Command Line: */
-        for (Property property : properties) {
-            System.out.println(property.toString());
+    public void viewProperties(){
+        /* Prints properties array */
+        for (int i = 0; i< properties.size(); i++){
+            System.out.println(properties.get(i).toString());
         }
     }
     public void payTax(Property p, int year){
         /* Take payment, create propertyTax, store in property.(Implement in PropertyTax)*/
-        ArrayList<PropertyTax> history = new ArrayList<PropertyTax>();
-        history = p.getTaxHistory();
-        for (PropertyTax propertyTax : history) {
-            if (propertyTax.getYear() == year) {
-                propertyTax.setPaid(true);
+        ArrayList<PropertyTax> history = p.getTaxHistory();
+        for (int i = 0; i < history.size(); i++){
+            if(history.get(i).getYear() == year){
+                history.get(i).setPaid();
             }
         }
-        /* Need to add to CSV file */
+
+
+
+        /* Need to add to CSV file*/
     }
     public void viewPaidTax(){
-        /* Return list of propertyTax*/
-        for (Property p : properties) {
+        /* Prints all PAID Taxes */
+        for (int i = 0; i< properties.size(); i++){
+            Property p = properties.get(i);
             System.out.println("\nTaxes Paid for Property at " + p.getAddress());
-            ArrayList<PropertyTax> history = new ArrayList<PropertyTax>();
-            history = p.getTaxHistory();
-            for (PropertyTax tax : history) {
-                if (tax.getPaid() == true) {
+            ArrayList<PropertyTax> history = p.getTaxHistory();
+            for (int y = 0; y < history.size(); y++){
+                PropertyTax tax = history.get(y);
+                if (tax.getPaid()){
                     System.out.println("\nYear: " + tax.getYear() + "\nTax Paid: " + tax.getTax());
                 }
             }
         }
     }
-    public void viewUnpaidTax(){
-        /* Return list of propertyTax*/
-        for (Property p : properties) {
-            System.out.println("\nTaxes Unpaid for Property at " + p.getAddress());
-            ArrayList<PropertyTax> history = new ArrayList<PropertyTax>();
-            history = p.getTaxHistory();
-            for (PropertyTax tax : history) {
-                if (tax.getPaid() == false) {
-                    System.out.println("\nYear: " + tax.getYear() + "\nTax Unpaid: " + tax.getTax());
+    public void viewDueTax(){
+        /* Prints THIS YEARS DUE Taxes */
+        for (int i = 0; i< properties.size(); i++){
+            Property p = properties.get(i);
+            System.out.println("\nTax Due this year for Property at " + p.getAddress());
+            ArrayList<PropertyTax> history = p.getTaxHistory();
+            for (int y = 0; y < history.size(); y++){
+                PropertyTax tax = history.get(y);
+                if (!tax.getPaid()&&(tax.getYear() == 2020)){
+                    System.out.println("\nTax Due: " + tax.getTax());
                 }
             }
         }
     }
     public void viewOverdueTax(){
-        /* Show overdue tax on property*/
-            /* Show overdue tax on property*/
-        for (Property p : properties) {
+        /* Prints all OVERDUE Taxes, i.e NOT this years */
+        for (int i = 0; i< properties.size(); i++){
+            Property p = properties.get(i);
             System.out.println("\nTaxes Overdue for Property at " + p.getAddress());
-            ArrayList<PropertyTax> history = new ArrayList<PropertyTax>();
-            history = p.getTaxHistory();
-            for (int y = 0; y < history.size(); y++) {
-                System.out.println(history.size());
+            ArrayList<PropertyTax> history = p.getTaxHistory();
+            for (int y = 0; y < history.size(); y++){
                 PropertyTax tax = history.get(y);
-                if ((tax.getPaid() == false) && (tax.getYear() != 2020)) {
+                if ((!tax.getPaid())&&(tax.getYear() != 2020)){
                     System.out.println("\nYear: " + tax.getYear() + "\nTax Overdue: " + tax.getTax());
                 }
             }
         }
-        }
+    }
     public void balancingStatement(int year){
-        /* Shows amount of tax paid for specific year*/
-        for (Property p : properties) {
-            ArrayList<PropertyTax> history = new ArrayList<PropertyTax>();
-            history = p.getTaxHistory();
-            for (PropertyTax tax : history) {
-                if (tax.getPaid() == true && tax.getYear() == year) {
-                    System.out.println("\nTax Paid in this year: " + tax.getYear() + ", "
-                            + tax.getTax());
+        /* Shows amount of tax, paid and unpaid, for specific year*/
+        double unpaid = 0;
+        double paid = 0;
+        for (int i = 0; i< properties.size(); i++){
+            Property p = properties.get(i);
+            ArrayList<PropertyTax> history = p.getTaxHistory();
+            for (int j = 0; j < history.size(); j++){
+                PropertyTax tax = history.get(j);
+                if (tax.getYear() == year){
+                    if (tax.getPaid()){
+                        paid = paid + tax.getTax();
+                    } else {
+                        unpaid = unpaid + tax.getTax();
+                    }
                 }
             }
         }
+        System.out.println("\nPaid tax in this year: " + year + ", " + paid);
+        System.out.println("\nUnpaid tax in this year: " + year + ", " + unpaid);
+    }
+    public void balancingStatement(Property p){
+        /* Shows amount of tax paid for specific Property*/
+        ArrayList<PropertyTax> history = p.getTaxHistory();
+        System.out.println("\nAll taxes for Property at " + p.getAddress());
+        for (int y = 0; y < history.size(); y++){
+            PropertyTax tax = history.get(y);
+            if (tax.getPaid()){
+                System.out.println("\nYear: " + tax.getYear() + "\nTax Paid: " + tax.getTax());
+            } else {
+               System.out.println("\nYear: " + tax.getYear() + "\nTax Due: " + tax.getTax());
+            }
+        }    
     }
 }
