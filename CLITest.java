07fc02e756e6a1f0784d4d2a70a494881fe61ac0
Danielz110 <64483972+Danielz110@users.noreply.github.com>
2020-12-13 00:31:35 +0000

    Update CLITest.java

--- CLITest.java
+++ CLITest.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 public class CLITest {
-    public static void main(String[] args)
+    public CLITest()
             throws IOException {
         Management manager = new Management();
         CLI menu = new CLI();
